5daf382d62acc69be387aaad9f16809c6ccafeff
omh02033 <omh02033@gmail.com>
2022-02-16 16:29:11 +0900

    [feat] add Login Func

--- BackEnd/src/interfaces/db.ts
+++ BackEnd/src/interfaces/db.ts
@@ -2,7 +2,12 @@ export interface DBUser {
     uid: number;
     userId: string;
     password?: string;
+    passwordSalt?: string;
     signUpPath: string;
     email: string;
     name: string;
+    points: number;
+    regiTime: Date;
+    temporaryPassword: string;
+    forgetPwdStatus: string;
 }
--- BackEnd/src/routes/auth.ts
+++ BackEnd/src/routes/auth.ts
@@ -3,11 +3,30 @@ import knex from '../config/db';
 import jwt from 'jsonwebtoken';
 import CONF from '../config';
 import { DBUser, tokenInterface } from '../interfaces';
+import crypto from 'crypto';
 
 const router = Router();
 
 
 router
+.post('/login', async (req: Request, res: Response) => {
+  const { uid, upw } = req.body;
+  if(!uid || !upw) return res.status(400).json({ success: false, message: "필드를 확인해주세요." });
+
+  const [user]: Array<DBUser> = await knex('users').where({ userId: uid });
+  if(!user) return res.status(400).json({ success: false, message: "아이디 또는 비밀번호가 일치하지 않습니다." });
+
+  const hashPassword = crypto.createHash('sha512').update(upw + user.passwordSalt).digest('hex');
+  if(user.password === hashPassword) {
+    const token = await jwt.sign({
+      uid: user.uid,
+      userId: user.userId,
+      name: user.name
+    }, CONF.jwt.key as string, CONF.jwt.options);
+    return res.status(200).json({ success: true, token });
+  } else return res.status(400).json({ success: false, message: "아이디 또는 비밀번호가 일치하지 않습니다." });
+})
+
 .get('/isLogin', async (req: Request, res: Response) => {
   const token = req.headers.authorization;
   if (!token) return res.status(200).json({
