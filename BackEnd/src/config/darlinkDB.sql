6f3aab8ca2341a73ec02839692d1b8c3c4e9ebad
omh02033 <omh02033@gmail.com>
2022-05-02 22:40:59 +0900

    fix: Dockerfile

--- BackEnd/src/config/darlinkDB.sql
+++ BackEnd/src/config/darlinkDB.sql
@@ -1,84 +1,59 @@
--- --------------------------------------------------------
--- 호스트:                          127.0.0.1
--- 서버 버전:                        10.4.17-MariaDB - mariadb.org binary distribution
--- 서버 OS:                        Win64
--- HeidiSQL 버전:                  11.0.0.5919
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
--- darlink 데이터베이스 구조 내보내기
-CREATE DATABASE IF NOT EXISTS `darlink` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `darlink`;
-
--- 테이블 darlink.admin 구조 내보내기
-CREATE TABLE IF NOT EXISTS `admin` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `user_uid` int(11) NOT NULL,
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `uid` int NOT NULL AUTO_INCREMENT,
+  `userUid` int NOT NULL,
   PRIMARY KEY (`uid`),
-  KEY `FK1_USER_UID` (`user_uid`),
-  CONSTRAINT `FK1_USER_UID` FOREIGN KEY (`user_uid`) REFERENCES `users` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `FK1_USER_UID` (`userUid`),
+  CONSTRAINT `FK1_USER_UID` FOREIGN KEY (`userUid`) REFERENCES `users` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS `likes` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `userUid` int(11) NOT NULL,
-  `linkUid` int(11) NOT NULL,
-  PRIMARY KEY (`uid`),
-  KEY `FK1_USER_UID2` (`userUid`),
-  CONSTRAINT `FK1_USER_UID2` FOREIGN KEY (`userUid`) REFERENCES `users` (`uid`),
-  KEY `FK2_LINK_UID` (`linkUid`),
-  CONSTRAINT `FK2_LINK_UID` FOREIGN KEY (`linkUid`) REFERENCES `links` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- 내보낼 데이터가 선택되어 있지 않습니다.
-
--- 테이블 darlink.attendance 구조 내보내기
-CREATE TABLE IF NOT EXISTS `attendance` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `user_uid` int(11) DEFAULT NULL,
-  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+DROP TABLE IF EXISTS `attendance`;
+CREATE TABLE `attendance` (
+  `uid` int NOT NULL AUTO_INCREMENT,
+  `userUid` int DEFAULT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `day` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`uid`),
-  KEY `FK1_USER_UID1` (`user_uid`),
-  CONSTRAINT `FK1_USER_UID1` FOREIGN KEY (`user_uid`) REFERENCES `users` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `FK1_USER_UID1` (`userUid`),
+  CONSTRAINT `FK1_USER_UID1` FOREIGN KEY (`userUid`) REFERENCES `users` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE `likes` (
+  `uid` int NOT NULL AUTO_INCREMENT,
+  `userUid` int NOT NULL,
+  `linkUid` int NOT NULL,
+  PRIMARY KEY (`uid`),
+  KEY `FK1_USER_UID2` (`userUid`),
+  KEY `FK2_LINK_UID` (`linkUid`),
+  CONSTRAINT `FK1_USER_UID2` FOREIGN KEY (`userUid`) REFERENCES `users` (`uid`),
+  CONSTRAINT `FK2_LINK_UID` FOREIGN KEY (`linkUid`) REFERENCES `links` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 테이블 darlink.links 구조 내보내기
-CREATE TABLE IF NOT EXISTS `links` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `url` text DEFAULT NULL,
-  `image` text DEFAULT NULL,
+DROP TABLE IF EXISTS `links`;
+CREATE TABLE `links` (
+  `uid` int NOT NULL AUTO_INCREMENT,
+  `url` text,
+  `image` text,
   `tag` varchar(200) DEFAULT NULL,
-  `likeNum` int(10) unsigned NOT NULL DEFAULT 0,
+  `field` varchar(200) DEFAULT NULL,
+  `location` varchar(200) DEFAULT NULL,
+  `title` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
-
--- 테이블 darlink.users 구조 내보내기
-CREATE TABLE IF NOT EXISTS `users` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `uid` int NOT NULL AUTO_INCREMENT,
   `userId` varchar(200) DEFAULT NULL,
-  `password` text DEFAULT NULL,
+  `password` text,
   `email` varchar(200) DEFAULT NULL,
   `name` varchar(200) DEFAULT NULL,
   `signUpPath` varchar(200) DEFAULT NULL,
-  `likes` mediumtext DEFAULT NULL,
-  `points` int(11) NOT NULL DEFAULT 0,
-  `regiTime` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `points` int NOT NULL DEFAULT '0',
+  `regiTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `forgetPwdStatus` varchar(20) NOT NULL,
+  `passwordSalt` varchar(200) DEFAULT NULL,
+  `temporaryPassword` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- 내보낼 데이터가 선택되어 있지 않습니다.
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
